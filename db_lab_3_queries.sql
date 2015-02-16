bc9cba6a11144d027df58781e11e0d23d2512473
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-02-16 17:15:02 -0500

    Lab 3 done, pending a double-check of the answers

---
+++ db_lab_3_queries.sql
@@ -0,0 +1,51 @@
+﻿-- John Paul Welsh
+-- Database Systems Lab #3
+
+-- Question 1
+select ordno, dollars
+from   orders;
+
+-- Question 2
+select name, city
+from   agents
+where  name = 'Smith';
+
+-- Question 3
+select pid, name, priceUSD
+from   products
+where  quantity > 200000;
+
+-- Question 4
+select name, city
+from   customers
+where  city = 'Dallas';
+
+-- Question 5
+select name
+from   agents
+where  city != 'New York'
+  and  city != 'Tokyo';
+
+-- Question 6
+select *
+from   products
+where  city != 'Dallas'
+  and  city != 'Duluth'
+  and  priceusd >= 1.00;
+
+-- Question 7
+select *
+from   orders
+where  mon = 'jan'
+   or  mon = 'may';
+
+-- Question 8
+select *
+from   orders
+where  mon = 'feb'
+  and  dollars > 500.00;
+
+-- Question 9
+select *
+from   orders
+where  cid = 'c005';
