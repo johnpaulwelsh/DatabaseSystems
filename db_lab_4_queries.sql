07d4c576dac30de2dba14b593550822627d594c9
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-02-23 10:38:09 -0500

    Renamed lab 2 files, first query of lab 4 done

---
+++ db_lab_4_queries.sql
@@ -0,0 +1,12 @@
+﻿-- John Paul Welsh
+-- Database Systems Lab #4
+
+-- Question 1
+select city
+from   agents
+where  aid in (select aid
+               from   orders
+               where  orders.cid = 'c006'
+              )
+
+-- Question 2
