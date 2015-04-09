d2951ab45af77f8bcf1bf2d4c4916bb988bffd53
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-09 17:41:34 -0400

    Working it out on paper, saving work

--- lab_8/db_lab_8_sql.sql
+++ lab_8/db_lab_8_sql.sql
@@ -8,8 +8,8 @@ CREATE TABLE suppliers (
  primary key(sid)
 );
 
--- Addresses --
-CREATE TABLE addresses (
+-- Supplier Location Information --
+CREATE TABLE supplierLocations (
   sid         char(4) not null references suppliers(sid),
   zip         integer not null references suppliers(zip),
   address     text,
@@ -18,30 +18,22 @@ CREATE TABLE addresses (
   primary key(sid,zip)
 );
 
--- Merchandise --
+-- Clothing Items --
 CREATE TABLE merchandise (
   sku         char(4) not null,
   description text,
-  priceUSD    numeric(10,2),
   quantity    integer,
+  priceUSD    numeric(10,2),
  primary key(sku)
-);
-
-CREATE TABLE skulists (
-  skulistno char(4) not null,
-  sku       char(4) not null references merchandise(sku),
-  ordno     char(4),
-  quantity  integer,
-  primary key(skulistno,sku)
 );
 
 -- Orders -- 
 CREATE TABLE orders (
-  ordno     char(4) not null,
-  sid       char(4) not null references suppliers(sid),
-  skulistno char(4) not null references skulists(skulistno),
-  orddate   date,
-  comments  text,
-  primary key(ordno)
+  
+);
+
+-- Lists of Items in Each Order --
+CREATE TABLE itemlists (
+
 );
 
