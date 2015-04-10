c85bb1efa814d1993f53fbb3c4a5a82c22aaf5d8
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-10 16:57:11 -0400

    Getting an error with my supplierlocations table

--- lab_8/db_lab_8_sql.sql
+++ lab_8/db_lab_8_sql.sql
@@ -1,3 +1,9 @@
+﻿DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS supplierlocations;
+DROP TABLE IF EXISTS merchandise;
+DROP TABLE IF EXISTS merchorders;
+DROP TABLE IF EXISTS itemlists;
+
 -- Suppliers --
 CREATE TABLE suppliers (
   sid         char(4) not null,
@@ -9,7 +15,7 @@ CREATE TABLE suppliers (
 );
 
 -- Supplier Location Information --
-CREATE TABLE supplierLocations (
+CREATE TABLE supplierlocations (
   sid         char(4) not null references suppliers(sid),
   zip         integer not null references suppliers(zip),
   address     text,
@@ -28,12 +34,21 @@ CREATE TABLE merchandise (
 );
 
 -- Orders -- 
-CREATE TABLE orders (
-  
+CREATE TABLE merchorders (
+  ordno     char(4) not null,
+  sid       char(4) references suppliers(sid),
+  listno    char(4) references itemlists(listno),
+  comments  text,
+  orderdate date,
+  primary key(ordno)
 );
 
 -- Lists of Items in Each Order --
 CREATE TABLE itemlists (
-
+  listno char(4) not null,
+  sku    char(4) not null references merchandise(sku),
+  quantity integer,
+  price numeric(10,2),
+  primary key(listno,sku)
 );
 
