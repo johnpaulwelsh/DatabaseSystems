31e79b422d44a2af77780d3dd0de22643e7ec1c4
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-20 16:55:51 -0400

    Began lab 10: just the template for the scripts

---
+++ lab_10/db_lab_10_sql.sql
@@ -0,0 +1,27 @@
+﻿-- John Paul Welsh
+-- Lab 10
+
+-- Question 1
+-- Returns the immediate prerequisites for the passed-in course number.
+create or replace function function PreReqsFor(courseNum) returns refcursor
+as $$
+declare
+   -- 
+begin
+   open $1 for select * from someTable;
+   return $1;
+end;
+$$ language plpgsql;
+
+-- Question 2
+-- Returns the courses for which the passed-in course number is an
+-- immediate pre-requisite.
+create or replace function function IsPreReqFor(courseNum) returns refcursor
+as $$
+declare
+   -- 
+begin
+   open $1 for select * from someTable;
+   return $1;
+end;
+$$ language plpgsql;
