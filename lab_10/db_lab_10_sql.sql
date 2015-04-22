7b47504408c6f99594d0ddf60c6296dda76c242d
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-21 21:27:20 -0400

    Almost have the first function in lab 10 working: problem with not using refcursor as a parameter

--- lab_10/db_lab_10_sql.sql
+++ lab_10/db_lab_10_sql.sql
@@ -3,8 +3,30 @@
 
 -- Question 1
 -- Returns the immediate prerequisites for the passed-in course number.
-create or replace function function PreReqsFor(courseNum) returns refcursor
-as $$
+create or replace function PreReqsFor(integer) returns refcursor as
+$$
+declare
+  searchNum integer := $1;
+  results   refcursor;
+begin
+  open results for
+    select *
+      from courses c
+     where c.num in (select p.prereqnum
+                       from prerequisites p
+                      where p.coursenum = searchNum);
+  return results;
+end;
+$$ language plpgsql;
+
+select PreReqsFor(308);
+fetch all from results;
+
+-- Question 2
+-- Returns the courses for which the passed-in course number is an
+-- immediate pre-requisite.
+create or replace function IsPreReqFor(searchNum) returns refcursor as
+$$
 declare
    -- 
 begin
@@ -13,15 +35,8 @@ begin
 end;
 $$ language plpgsql;
 
--- Question 2
--- Returns the courses for which the passed-in course number is an
--- immediate pre-requisite.
-create or replace function function IsPreReqFor(courseNum) returns refcursor
-as $$
-declare
-   -- 
-begin
-   open $1 for select * from someTable;
-   return $1;
-end;
-$$ language plpgsql;
+select *
+from prerequisites
+
+select *
+from courses
