08488206861eba2d0eab7626b73a132ff5995d7c
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-23 10:54:49 -0400

    Lab 10: both functions work! With explicit params

--- lab_10/db_lab_10_sql.sql
+++ lab_10/db_lab_10_sql.sql
@@ -3,11 +3,11 @@
 
 -- Question 1
 -- Returns the immediate prerequisites for the passed-in course number.
-create or replace function PreReqsFor(integer) returns refcursor as
+create or replace function PreReqsFor(integer, refcursor) returns refcursor as
 $$
 declare
-  searchNum integer := $1;
-  results   refcursor;
+  searchNum integer   := $1;
+  results   refcursor := $2;
 begin
   open results for
     select *
@@ -25,11 +25,11 @@ fetch all from results;
 -- Question 2
 -- Returns the courses for which the passed-in course number is an
 -- immediate pre-requisite.
-create or replace function IsPreReqFor(integer) returns refcursor as
+create or replace function IsPreReqFor(integer, refcursor) returns refcursor as
 $$
 declare
-   searchNum integer := $1;
-   results   refcursor;
+   searchNum integer   := $1;
+   results   refcursor := $2;
 begin
    open results for
      select *
@@ -43,9 +43,3 @@ $$ language plpgsql;
 
 select IsPreReqFor(308, 'results');
 fetch all from results;
-
-select *
-from prerequisites
-
-select *
-from courses
