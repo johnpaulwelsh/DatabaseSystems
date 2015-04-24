66738b519094eaafb0a269d1144f69b0489e12c7
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-23 21:14:17 -0400

    Empire: finished adding tables (maybe?); still no testing

--- design_project/db_design_project.sql
+++ design_project/db_design_project.sql
@@ -31,35 +31,6 @@ CREATE TABLE beings (
   primary key(bid)
 );
 
-CREATE TABLE employees (
-  bid           char(4) references beings(bid),
-  stationbaseid char(5) references bases(baseid),
-  salaryGCS     numeric(10,2),
-  primary key(bid)
-);
-
-CREATE TABLE engineers (
-  bid           char(4) references beings(bid),
-  primary key(bid)
-);
-
-CREATE TABLE administrators (
-  bid           char(4) references beings(bid),
-  primary key(bid)
-);
-
-CREATE TABLE mechanics (
-  bid           char(4) references beings(bid),
-  isrighthanded boolean,
-  primary key(bid)
-);
-
-CREATE TABLE soldiers (
-  bid     char(4) references beings(bid),
-  isclone boolean,
-  primary key(bid)
-);
-
 CREATE TABLE jedi (
   bid        char(4) references beings(bid),
   sabercolor text, -- MAKE THIS A RESTRICTED DOMAIN
@@ -67,9 +38,64 @@ CREATE TABLE jedi (
   primary key(bid)
 );
 
-CREATE TABLE spies (
-  bid         char(4) references beings(bid),
-  focusplanet text references planets(planetname),
-  specialty   text,
+CREATE TABLE sith (
+  bid        char(4) references beings(bid),
+  sabercolor text, -- MAKE THIS A RESTRICTED DOMAIN
+  rank       text, -- THIS TOO
   primary key(bid)
+);
+
+CREATE TABLE employees (
+  bid           char(4) references beings(bid),
+  eid           char(4),
+  stationbaseid char(5) references bases(baseid),
+  salaryGCS     numeric(10,2),
+  primary key(eid)
+);
+
+CREATE TABLE administrators (
+  eid char(4) references employees(eid),
+  primary key(bid)
+);
+
+CREATE TABLE soldiers (
+  eid     char(4) references employees(eid),
+  isclone boolean,
+  primary key(bid)
+);
+
+CREATE TABLE spies (
+  eid         char(4) references employees(eid),
+  focusplanet text references planets(planetname),
+  primary key(bid)
+);
+
+CREATE TABLE organizations (
+  orgid   char(3),
+  orgname text,
+  primary key(orgid)
+);
+
+CREATE TABLE alliedOrgs (
+  orgid            char(3) references organizations(orgid),
+  lastactivitydate date,
+  primary key(orgid)
+);
+
+CREATE TABLE enemyOrgs (
+  orgid          char(3) references organizations(orgid),
+  lasttransmdate date,
+  primary key(orgid)
+);
+
+CREATE TABLE alliedLeaders (
+  bid   char(4) references beings(bid),
+  orgid char(3) references organizations(orgid),
+  primary key(bid,orgid)
+);
+
+CREATE TABLE enemyLeaders (
+  bid   char(4) references beings(bid),
+  orgid char(3) references organizations(orgid),
+  primary key(bid,orgid)
 );
