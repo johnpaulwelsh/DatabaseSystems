c939265a36583ca08cd9edf148b05b33983148e6
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-04-25 14:19:20 -0400

    Empire: thus begins endlessly inserting data

--- design_project/db_design_project.sql
+++ design_project/db_design_project.sql
@@ -14,21 +14,20 @@ DROP TABLE IF EXISTS soldiers;
 DROP TABLE IF EXISTS spies;
 DROP TABLE IF EXISTS administrators;
 DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS basesOnPlanets;
 DROP TABLE IF EXISTS bases;
 DROP TABLE IF EXISTS beings;
-DROP TABLE IF EXISTS planets;
+DROP TABLE IF EXISTS habitats;
 
 -- Create Statements --
 -----------------------
 
--- Planets --
-CREATE TABLE planets (
-  planetname text not null,
+-- Habitats (equivalent to planets, but inclusive of moons) --
+CREATE TABLE habitats (
+  habitatname text not null,
   region     text,
   sector     text,
-  system     text,
-  primary key(planetname)
+  sys        text,
+  primary key(habitatname)
 );
 
 -- Living Beings (equivalent to people, but more inclusive) --
@@ -36,7 +35,7 @@ CREATE TABLE beings (
   bid        char(4),
   lastname   text,
   firstname  text,
-  homeplanet text references planets(planetname),
+  homehabitat text references habitats(habitatname),
   species    text,
   birthdate  date,
   primary key(bid)
@@ -60,7 +59,9 @@ CREATE TABLE sith (
 
 -- Empire bases --
 CREATE TABLE bases (
-  baseid       char(5) not null,
+  baseid      char(5) not null,
+  basename    text,
+  habitatname text,
   primary key(baseid)
 );
 
@@ -90,15 +91,8 @@ CREATE TABLE soldiers (
 -- Spies for the Empire --
 CREATE TABLE spies (
   spyid       char(4) references employees(eid),
-  focusplanet text    references planets(planetname),
+  focushabitat text    references habitats(habitatname),
   primary key(spyid)
-);
-
--- A cross reference for which bases are on which planets --
-CREATE TABLE basesOnPlanets (
-  baseid     char(5) not null references bases(baseid),
-  planetname text    not null references planets(planetname),
-  primary key(baseid, planetname)
 );
 
 -- Established organizations all across the galaxy --
@@ -110,15 +104,15 @@ CREATE TABLE organizations (
 
 -- Organizations that are allied with the Empire --
 CREATE TABLE alliedOrgs (
-  orgid            char(3) references organizations(orgid),
-  lastactivitydate date,
+  orgid          char(3) references organizations(orgid),
+  lasttransmdate date,
   primary key(orgid)
 );
 
 -- Organizations that are enemies to the Empire --
 CREATE TABLE enemyOrgs (
-  orgid          char(3) references organizations(orgid),
-  lasttransmdate date,
+  orgid            char(3) references organizations(orgid),
+  lastactivitydate date,
   primary key(orgid)
 );
 
@@ -136,10 +130,70 @@ CREATE TABLE enemyLeaders (
   primary key(bid,orgid)
 );
 
+-- Sample Data --
+-----------------
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Alderaan', 'Core Worlds', 'Alderaan', 'Alderaan');
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Tatooine', 'Outer Rim Territories', 'Arkanis', 'Tatoo');
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Yavin 4', 'Outer Rim Territories', 'Gordian Reach', 'Yavin');
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Kashyyyk', 'Mytaranor', 'Kashyyyk', 'Kashyyyk');
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Eriadu', 'Outer Rim Territories', 'Seswanna', 'Eriadu');
+INSERT INTO habitats(habitatname, region, sector, sys)
+             VALUES('Naboo', 'Mid Rim', 'Chommell', 'Naboo');
+
+INSERT INTO bases(baseid, basename, habitatname)
+           VALUES('b0001', 'Death Star', null);
+INSERT INTO bases(baseid)
+           VALUES('b0002', 'Fondor II', 'Eriadu');
+INSERT INTO bases(baseid)
+           VALUES('b0003', 'Tremor Base', 'Eriadu');
+INSERT INTO bases(baseid)
+           VALUES('b0004', 'Kashyyyk Imperial Base', 'Kashyyyk');
+INSERT INTO bases(baseid)
+           VALUES('b0005', 'Alpha Outpost', 'Tatooine');
+
+INSERT INTO organizations(orgid, orgname)
+                   VALUES('o01', 'Raider Allegiance');
+INSERT INTO organizations(orgid, orgname)
+                   VALUES('o02', 'Senatorial Committee');
+INSERT INTO organizations(orgid, orgname)
+                   VALUES('o03', 'Fleet General Academy');
+INSERT INTO organizations(orgid, orgname)
+                   VALUES('o04', 'Rebel Alliance');
+INSERT INTO organizations(orgid, orgname)
+                   VALUES('o05', 'Hutt Gang');
+
+INSERT INTO alliedOrgs(orgid, lasttransmdate)
+                   VALUES('o05', '2012-10-06');
+INSERT INTO alliedOrgs(orgid, lasttransmdate)
+                   VALUES('o03', '2015-01-01');
+
+INSERT INTO enemyOrgs(orgid, lastactivitydate)
+                   VALUES('o04', '2015-02-03');
+INSERT INTO enemyOrgs(orgid, lastactivitydate)
+                   VALUES('o02', '2013-12-21');
+
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x001', 'Luke', 'Skywalker', 'Tatooine', 'Human', '1965-06-13');
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x002', 'Anakin', 'Skywalker', 'Tatooine', 'Human', '1941-02-02');
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x003', 'Chewbacca', null, 'Kashyyyk', 'Wookie', '1950-04-18');
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x004', 'Wilhuff', 'Tarkin', 'Eriadu', 'Human', '1920-10-01');
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x005', 'Leia', 'Organa', 'Alderaan', 'Human', '1965-06-13');
+INSERT INTO beings(bid, firstname, lastname, homehabitat, species, birthdate)
+            VALUES('x006', null, 'Palpatine', 'Naboo', 'Human', '1919-12-01');
+
+
+
 -- Views --
 -----------
-
-
 
 -- Reports --
 -------------
